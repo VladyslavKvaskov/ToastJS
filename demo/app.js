30c332919d906293a411640d5dc0a997ef35ba45
Vladislav Kvasko <vladislav.kvasko@bugfreakmedia.com>
2020-10-19 12:12:03 +0300

    added confirm, deny, and cancel buttons

--- demo/app.js
+++ demo/app.js
@@ -1,11 +1,11 @@
-let textArr = ['Hi there!', 'What\'s up?', 'How are you?', 'Cool toasts!!!'];
-let statusArr = ['success',
+const textArr = ['Hi there!', 'What\'s up?', 'How are you?', 'Cool toasts!!!'];
+const statusArr = ['success',
   'info',
   'warning',
   'error',
   'default'
 ]
-let positionArr = [
+const positionArr = [
   'left',
   'right',
   'bottom',
@@ -18,35 +18,79 @@ let positionArr = [
 ]
 
 function showToaster() {
+  const toastPosition = positionArr[getRndInteger(0, 9)];
   new Toaster({
     status: statusArr[getRndInteger(0, 5)],
     title: 'ToastJS',
     // text: textArr[getRndInteger(0, 3)],
     html: `<div class="open"></div>`,
     timing: getRndInteger(3000, 15000),
-    showDuration: 500,
-    hideDuration: 700,
-    position: positionArr[getRndInteger(0, 9)],
+    showDuration: 700,
+    hideDuration: 1000,
+    position: toastPosition,
     cssClass: ['newtoast1', 'newtoast2'],
     titleIcon: 'https://static.codepen.io/assets/favicon/favicon-aec34940fbc1a6e787974dcd360f2c6b63348d4b1f4e06c77743096d55480f33.ico',
     showTimer: true,
-    interactive: false,
-    closeOnOutsideClick: false,
+    interactive: true,
     showCloseBttn: true,
-    onOpen: (toastMessage) => {
-      toastMessage.querySelector('.open').innerText = textArr[getRndInteger(0, 3)];
+    showConfirmBttn: true,
+    showDenyBttn: true,
+    showCancelBttn: true,
+    confirmBttnText: 'Confirm',
+    denyBttnText: 'Deny',
+    cancelBttnText: 'Cancel',
+    confirmBttnClick: (toast) => {
+      new Toaster({
+        text: 'You\'ve confirmed the prompt',
+        timing: 5000,
+        showTimer: true,
+        status: 'info',
+        position: toastPosition,
+        closeOnOutsideClick: true
+      });
+
+      toast.delete();
     },
-    onClose: (toastMessage) => {
-      toastMessage.querySelector('.open').innerText = 'Goodbye!';
+    denyBttnClick: (toast) => {
+      new Toaster({
+        text: 'You\'ve denied the prompt.',
+        timing: 5000,
+        showTimer: true,
+        status: 'error',
+        position: toastPosition,
+        closeOnOutsideClick: true
+      });
+
+      toast.delete();
+    },
+    cancelBttnClick: (toast) => {
+      new Toaster({
+        text: 'You\'ve cancelled the prompt.',
+        timing: 5000,
+        showTimer: true,
+        position: toastPosition,
+        closeOnOutsideClick: true
+      });
+
+      toast.delete();
+    },
+    onOpen: (toast) => {
+      toast.querySelector('.open').innerText = textArr[getRndInteger(0, 3)];
+    },
+    onClose: (toast) => {
+      toast.querySelector('.open').innerText = 'Goodbye!';
+      showToaster();
     }
   });
+
   setTimeout(() => {
-    showToaster();
+    // showToaster();
   }, getRndInteger(3000, 15000));
 }
 
 showToaster();
-showToaster();
+// showToaster();
+
 
 function getRndInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
